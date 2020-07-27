f0281ec4dd00edd6a4931951a2c187ec94ff55fc
Arthur Zopellaro <arthurazsoares@gmail.com>
2020-07-27 01:17:49 -0300

    Add unsigned int packer/unpacker

---
+++ asn/unsigned_integer.py
@@ -0,0 +1,57 @@
+from struct import pack as s_pack, unpack as s_unpack
+
+U8 = 255
+U16 = 65535
+U24 = 16777215
+U32 = 4294967295
+
+
+class UnsignedInt:
+    
+    TAG = b'\x86'
+
+    @staticmethod
+    def pack(value):
+        if value is None:
+            return UnsignedInt.TAG + b'\x00'
+        elif value < 0:
+            raise ValueError('Unsigned integer cannot be negative')
+        elif value <= U8:
+            return UnsignedInt.TAG + b'\x01' + s_pack('>B', value)
+        elif value <= U16:
+            return UnsignedInt.TAG + b'\x02' + s_pack('>H', value)
+        elif value <= U24:
+            return UnsignedInt.TAG + b'\x03' + s_pack('>I', value)[1:]
+        elif value <= U32:
+            return UnsignedInt.TAG + b'\x04' + s_pack('>I', value)
+        raise ValueError('Unsigned integer out of supported range')
+
+    @staticmethod
+    def unpack(value):
+        tag = value[0:1]
+        try:
+            size = value[1]
+        except IndexError:
+            raise ValueError(f'{value} is missing length field')
+        number = value[2:]
+
+        if tag != UnsignedInt.TAG:
+            raise TypeError(f'{value} is not an unsigned integer')
+
+        if size < len(number):
+            raise ValueError(f'{value} seems to be incomplete')
+
+        if size > len(number):
+            raise ValueError(f'{value} seems to have extra information')
+
+        if size == 0:
+            return None
+        elif size == 1:
+            return s_unpack('>B', number)[0]
+        elif size == 2:
+            return s_unpack('>H', number)[0]
+        elif size == 3:
+            return s_unpack('>I', b'\x00' + number)[0]
+        elif size == 4:
+            return s_unpack('>I', number)[0]
+        raise ValueError('Unsigned integer out of supported range')
---
+++ tests/test_unsigned_integer.py
@@ -0,0 +1,64 @@
+import unittest
+from asn.unsigned_integer import UnsignedInt
+
+
+class TestIntegerPack(unittest.TestCase):
+    def test_pack_size_0(self):
+        self.assertEqual(UnsignedInt.pack(None), b'\x86\x00')
+
+    def test_pack_size_1(self):
+        self.assertEqual(UnsignedInt.pack(13), b'\x86\x01\x0D')
+
+    def test_pack_size_1_extreme(self):
+        self.assertEqual(UnsignedInt.pack(255), b'\x86\x01\xFF')
+
+    def test_pack_size_2(self):
+        self.assertEqual(UnsignedInt.pack(256), b'\x86\x02\x01\x00')
+
+    def test_pack_size_3(self):
+        self.assertEqual(UnsignedInt.pack(16777215), b'\x86\x03\xFF\xFF\xFF')
+
+    def test_pack_size_4(self):
+        self.assertEqual(UnsignedInt.pack(4294967295), b'\x86\x04\xFF\xFF\xFF\xFF')
+
+    def test_pack_negative(self):
+        self.assertRaises(ValueError, UnsignedInt.pack, -1)
+
+    def test_pack_out_of_range(self):
+        self.assertRaises(ValueError, UnsignedInt.pack, 4294967296)
+
+
+class TestIntegerUnpack(unittest.TestCase):
+    def test_unpack_size_0(self):
+        self.assertEqual(UnsignedInt.unpack(b'\x86\x00'), None)
+
+    def test_unpack_size_1(self):
+        self.assertEqual(UnsignedInt.unpack(b'\x86\x01\x00'), 0)
+
+    def test_unpack_size_1_extreme(self):
+        self.assertEqual(UnsignedInt.unpack(b'\x86\x01\xFF'), 255)
+
+    def test_unpack_size_2(self):
+        self.assertEqual(UnsignedInt.unpack(b'\x86\x02\x01\x00'), 256)
+
+    def test_unpack_size_3(self):
+        self.assertEqual(UnsignedInt.unpack(b'\x86\x03\xFF\xFF\xFF'), 16777215)
+
+    def test_unpack_size_4(self):
+        self.assertEqual(UnsignedInt.unpack(b'\x86\x04\xFF\xFF\xFF\xFF'), 4294967295)
+
+    def test_unpack_out_of_range(self):
+        self.assertRaises(ValueError, UnsignedInt.unpack, b'\x86\x05\x01\x00\x00\x00\x00')
+
+    def test_unpack_wrong_tag(self):
+        self.assertRaises(TypeError, UnsignedInt.unpack, b'\x87\x00')
+
+    def test_unpack_missing_size(self):
+        self.assertRaises(ValueError, UnsignedInt.unpack, b'\x86')
+
+    def test_unpack_wrong_size(self):
+        self.assertRaises(ValueError, UnsignedInt.unpack, b'\x86\x03\xFF\xFF\xFF\xFF')
+
+
+if __name__ == '__main__':
+    unittest.main()
