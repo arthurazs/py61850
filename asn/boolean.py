c777ae8cdc852cd76454705699e100cbdc7fafc2
Arthur Zopellaro <arthurazsoares@gmail.com>
2020-07-29 01:00:48 -0300

    Add boolean

---
+++ asn/boolean.py
@@ -0,0 +1,22 @@
+from asn.generic_asn import GenericASN
+
+
+class Boolean(GenericASN):
+
+    TAG = b'\x83'
+
+    @staticmethod
+    def pack(data):
+        if isinstance(data, bool):
+            return Boolean.generic_pack(b'\x0F' if data else b'\x00')
+        raise ValueError('Cannot pack non-boolean value')
+
+    @staticmethod
+    def unpack(data):
+        length, boolean = Boolean.generic_unpack(data)
+
+        if length == 0:
+            return None
+        elif length == 1:
+            return False if boolean == b'\x00' else True
+        raise ValueError('Boolean out of supported range')
