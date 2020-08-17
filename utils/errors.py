30991f487e087b4412658c05a9ace95f3a853803
Arthur Zopellaro <arthurazsoares@gmail.com>
2020-08-16 22:21:35 -0300

    Add possible TODO

--- utils/errors.py
+++ utils/errors.py
@@ -1,2 +1,4 @@
 def raise_type(argument_name, expected_type, real_type):
+    # NOTE maybe change to
+    # TypeError: int() argument must be a string, a bytes-like object or a number, not 'list'
     raise TypeError(f'expected {argument_name} to be {expected_type}, got {real_type} instead')
