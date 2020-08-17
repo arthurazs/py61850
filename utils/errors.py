3caa1967930c563e3af2916cf3fb9c0de211199f
Arthur Zopellaro <arthurazsoares@gmail.com>
2020-08-17 03:40:10 -0300

    Add type hinting

--- utils/errors.py
+++ utils/errors.py
@@ -1,4 +1,7 @@
-def raise_type(argument_name, expected_type, real_type):
+from typing import NoReturn
+
+
+def raise_type(argument_name: str, expected_type: type, real_type: type) -> NoReturn:
     # NOTE maybe change to
     # TypeError: int() argument must be a string, a bytes-like object or a number, not 'list'
     raise TypeError(f'expected {argument_name} to be {expected_type}, got {real_type} instead')
