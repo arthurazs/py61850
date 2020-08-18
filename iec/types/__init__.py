9c0503c559613a970b876d382c8d8ae0f685f81d
Arthur Zopellaro <arthurazsoares@gmail.com>
2020-08-17 22:44:12 -0300

    Make make it easier to import types
    e.g., `from iec.types import Boolean`

---
+++ iec/types/__init__.py
@@ -0,0 +1,5 @@
+from iec.types.boolean import Boolean
+from iec.types.floating_point import FloatingPoint
+from iec.types.signed_integer import SignedInt
+from iec.types.unsigned_integer import UnsignedInt
+from iec.types.visible_string import VisibleString
