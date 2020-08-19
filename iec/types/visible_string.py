cffef4245cc4e25dff1bcd9754f65d76a8aa9d79
Arthur Zopellaro <arthurazsoares@gmail.com>
2020-08-19 00:20:19 -0300

    Rework visible string

--- iec/types/visible_string.py
+++ iec/types/visible_string.py
@@ -1,29 +1,32 @@
+from typing import Optional, Union
+
 from iec.types.base import Base
+from utils.errors import raise_type
 
 
 class VisibleString(Base):
 
-    TAG = b'\x8A'
+    def __init__(self, value: Union[str, bytes]) -> None:
+        raw_value, value = self._parse_value(value)
+        super().__init__(raw_tag=b'\x8A', raw_value=raw_value)
+        self._value = value
 
     @staticmethod
-    def pack(data, max_length=255):
-        # NOTE not sure if vstring is capped to 255
-        if isinstance(data, (str, type(None), )):
-            # NOTE should it be null terminated?
-            # IEDs does not seem to have null terminated strings
-            try:
-                string = data.encode('utf8')
-            except AttributeError:
-                return VisibleString.generic_pack(None)
-            if len(string) > max_length:
-                raise ValueError(f'data cannot be greater than {max_length}')
-            return VisibleString.generic_pack(string)
-        raise ValueError('Cannot pack non-string value')
-
-    @staticmethod
-    def unpack(data):
-        length, string = VisibleString.generic_unpack(data)
-
-        if length == 0:
+    def _encode_value(value: Optional[str]) -> Optional[bytes]:
+        if value is None:
             return None
-        return string.decode('utf8')
+        elif isinstance(value, str):
+            if len(value) == 0:
+                return None
+            elif 0 < len(value) <= 0xFF:
+                # TODO How should I limit other string lengths?
+                return value.encode('utf8')
+        raise_type('value', str, type(value))
+
+    @staticmethod
+    def _decode_value(value: bytes) -> Optional[str]:
+        if isinstance(value, bytes):
+            if 0 < len(value) <= 0xFF:
+                return value.decode('utf8')
+            raise ValueError('value out of supported length')
+        raise_type('value', bytes, type(value))
---
+++ tests/iec/types/test_visible_string.py
@@ -0,0 +1,153 @@
+from pytest import fixture, raises
+from iec.types import VisibleString
+
+
+@fixture
+def extreme():
+    return VisibleString('a' * 0xFF)
+
+
+@fixture
+def none():
+    return VisibleString(None)
+
+
+@fixture
+def string():
+    return VisibleString('string')
+
+
+@fixture
+def empty():
+    return VisibleString('')
+
+
+# === DECODE ===
+
+def test_byte_string_min_raw_value():
+    assert VisibleString(b'a').raw_value == b'a'
+
+
+def test_byte_string_min_value():
+    assert VisibleString(b'a').value == 'a'
+
+
+def test_byte_string_max_raw_value():
+    assert VisibleString(b'a' * 0xFF).raw_value == b'a' * 0xFF
+
+
+def test_byte_string_max_value():
+    assert VisibleString(b'a' * 0xFF).value == 'a' * 0xFF
+
+
+# === NONE ===
+
+def test_none_raw_value(none):
+    assert none.raw_value is None
+
+
+def test_none_value(none):
+    assert none.value is None
+
+
+def test_none_raw_length(none):
+    assert none.raw_length == b'\x00'
+
+
+def test_none_length(none):
+    assert none.length == 0
+
+
+def test_none_bytes(none):
+    assert bytes(none) == b'\x8A\x00'
+
+
+def test_none_len(none):
+    assert len(none) == 2
+
+
+# === EMPTY ===
+
+def test_empty_raw_value(empty):
+    assert empty.raw_value is None
+
+
+def test_empty_value(empty):
+    assert empty.value is None
+
+
+def test_empty_raw_length(empty):
+    assert empty.raw_length == b'\x00'
+
+
+def test_empty_length(empty):
+    assert empty.length == 0
+
+
+def test_empty_bytes(empty):
+    assert bytes(empty) == b'\x8A\x00'
+
+
+def test_empty_len(empty):
+    assert len(empty) == 2
+
+
+# === REGULAR VALUES ===
+
+def test_raw_tag(string):
+    assert string.raw_tag == b'\x8A'
+
+
+def test_tag(string):
+    assert string.tag == 'VisibleString'
+
+
+def test_raw_length(string):
+    assert string.raw_length == b'\x06'
+
+
+def test_length(string):
+    assert string.length == 6
+
+
+def test_bytes(string):
+    assert bytes(string) == b'\x8A\x06string'
+
+
+def test_len(string):
+    assert len(string) == 8
+
+
+# === EXTREME VALUES ===
+
+def test_extreme_raw_length(extreme):
+    assert extreme.raw_length == b'\x81\xFF'
+
+
+def test_extreme_length(extreme):
+    assert extreme.length == 0xFF
+
+
+def test_extreme_bytes(extreme):
+    assert bytes(extreme) == b'\x8A\x81\xFF' + (b'a' * 0xFF)
+
+
+def test_extreme_len(extreme):
+    assert len(extreme) == 3 + 0xFF
+
+
+# === EXCEPTIONS ===
+
+def test_encode_decode():
+    with raises(TypeError):
+        VisibleString(1)
+
+
+def test_decode_below():
+    with raises(ValueError):
+        VisibleString(b'')
+
+
+def test_decode_above():
+    with raises(ValueError):
+        VisibleString(b'a' * 0x1FF)
