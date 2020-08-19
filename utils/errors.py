a3b410c580bd2d5f4ef15019e95a3892657fdeae
Arthur Zopellaro <arthurazsoares@gmail.com>
2020-08-19 00:17:32 -0300

    Add new TODO

--- utils/errors.py
+++ utils/errors.py
@@ -2,6 +2,5 @@ from typing import NoReturn
 
 
 def raise_type(argument_name: str, expected_type: type, real_type: type) -> NoReturn:
-    # NOTE maybe change to
-    # TypeError: int() argument must be a string, a bytes-like object or a number, not 'list'
+    # TODO accept multiple expected types
     raise TypeError(f'expected {argument_name} to be {expected_type}, got {real_type} instead')
