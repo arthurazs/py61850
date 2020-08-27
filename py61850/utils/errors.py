c120a33643e6289898ebe8c2bc954f9f9a8c2ad2
Arthur Zopellaro <arthurazsoares@gmail.com>
2020-08-27 00:00:17 -0300

    Fix type hint

--- py61850/utils/errors.py
+++ py61850/utils/errors.py
@@ -1,6 +1,5 @@
-from typing import NoReturn
+from typing import NoReturn, Tuple, Union
 
 
-def raise_type(argument_name: str, expected_type: type, real_type: type) -> NoReturn:
-    # TODO accept multiple expected types
+def raise_type(argument_name: str, expected_type: Union[type, Tuple[type, ...]], real_type: type) -> NoReturn:
     raise TypeError(f'expected {argument_name} to be {expected_type}, got {real_type} instead')
